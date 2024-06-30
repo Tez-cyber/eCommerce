c64b7bb47bb5bdd68f1367de23e175e1c2ffe25e
Salami Azeez <salamitopeazeez@gmail.com>
2024-06-30 22:50:12 +0100

    Created Slider.jsx in components

--- src/components/Announcements.jsx
+++ src/components/Announcements.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Announcements = () => {
   return (
-    <div className="h-[30px] bg-teal-500 text-white">
+    <div className="h-[30px] bg-teal-500 text-white flex items-center justify-center text-sm font-semibold">
         Super deal! Free Shipping on Orders Over $50
     </div>
   )
