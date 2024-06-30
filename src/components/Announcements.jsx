17576b88c22de2d8e85f9faaaa7f6fed9c77e9a2
Salami Azeez <salamitopeazeez@gmail.com>
2024-06-30 22:47:32 +0100

    imported Announcement in Home.jsx

--- src/components/Announcements.jsx
+++ src/components/Announcements.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Announcements = () => {
   return (
-    <div>Announcements</div>
+    <div className="h-[30px] bg-teal-500 text-white">
+        Super deal! Free Shipping on Orders Over $50
+    </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Announcements from '../components/Announcements'
 
 const Home = () => {
   return (
     <div>
+        <Announcements />
         <Navbar />
     </div>
   )
