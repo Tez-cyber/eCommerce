52f4c4da51cd5e7939617bff546569d5d7309328
Salami Azeez <salamitopeazeez@gmail.com>
2024-06-30 23:19:30 +0100

    Added navigation for slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
-
+import ChevronLeftOutlinedIcon from '@mui/icons-material/ChevronLeftOutlined';
+import ChevronRightOutlinedIcon from '@mui/icons-material/ChevronRightOutlined';
 const Slider = () => {
   return (
-    <div>Slider</div>
+    <div className='relative w-full h-screen flex bg-orange-400'>
+        <div className='absolute top-0 cursor-pointer opacity-70 bottom-0 m-auto left-[10px] w-[50px] h-[50px] bg-[#fff7f7] rounded-full flex items-center justify-center'>
+            <ChevronLeftOutlinedIcon />
+        </div>
+        <div className='absolute top-0 cursor-pointer opacity-70 bottom-0 m-auto right-[10px] w-[50px] h-[50px] bg-[#fff7f7] rounded-full flex items-center justify-center'>
+            <ChevronRightOutlinedIcon />
+        </div>
+    </div>
   )
 }
 
