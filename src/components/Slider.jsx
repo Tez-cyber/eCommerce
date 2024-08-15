ae0bf863b5c1f6a455b1f98d4479e30735693c55
Salami Azeez <salamitopeazeez@gmail.com>
2024-08-15 11:54:58 +0100

    Imported useState and added key to mapped sliderItem array

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,61 +1,44 @@
 import ChevronLeftOutlinedIcon from '@mui/icons-material/ChevronLeftOutlined';
 import ChevronRightOutlinedIcon from '@mui/icons-material/ChevronRightOutlined';
 import Img1 from '../assets/img-2.jpg'
+import { sliderItems } from '../data';
+import { useState } from 'react';
 const Slider = () => {
-    const [slideIndex, setslideIndex] = useState(second)
+    const [slideIndex, setSlideIndex] = useState(0)
     const handleClick = (direction) => {
 
     }
-  return (
-    <div className='relative w-full h-screen flex overflow-hidden'>
-        <div
-            onClick={() => handleClick("left")}
-            className='absolute z-[2] top-0 cursor-pointer opacity-70 bottom-0 m-auto left-[10px] w-[50px] h-[50px] bg-[#fff7f7] rounded-full flex items-center justify-center'>
-            <ChevronLeftOutlinedIcon />
-        </div>
-        {/*==== Wrapper */}
-        <div className="h-full flex translate-x-[0vw]">
-            {/* ====First slide */}
-            <div className="flex items-center w-screen h-screen">
-                <div className="flex-1 h-full">
-                    <img className='h-[80%]' src="https://i.pinimg.com/originals/4a/35/63/4a3563e983a31a2001a1bfa882e2961e.png" alt="" />
-                </div>
-                <div className="flex-1 p-12 uppercase">
-                    <h1 className="text-7xl ">summer sale</h1>
-                    <p className="my-11 mx-0 text-xl font-semibold tracking-[3px]">dont compromise on style! get flat 30% off for new arrivals</p>
-                    <button className="uppercase p-3 border border-black cursor-pointer">shop now</button>
-                </div>
+    return (
+        <div className='relative w-full h-screen flex overflow-hidden'>
+            <div
+                onClick={() => handleClick("left")}
+                className='absolute top-0 cursor-pointer opacity-70 bottom-0 m-auto left-[10px] w-[50px] h-[50px] bg-[#fff7f7] rounded-full flex items-center justify-center'>
+                <ChevronLeftOutlinedIcon />
             </div>
-            {/* ====Second slide */}
-            <div className="flex items-center w-screen h-screen">
-                <div className="flex-1 h-full">
-                    <img className='h-[80%]' src="https://i.pinimg.com/originals/4a/35/63/4a3563e983a31a2001a1bfa882e2961e.png" alt="" />
-                </div>
-                <div className="flex-1 p-12 uppercase">
-                    <h1 className="text-7xl ">winter sale</h1>
-                    <p className="my-11 mx-0 text-xl font-semibold tracking-[3px]">dont compromise on style! get flat 30% off for new arrivals</p>
-                    <button className="uppercase p-3 border border-black cursor-pointer">shop now</button>
-                </div>
+            {/*==== Wrapper */}
+            <div className="h-full flex translate-x-[0vw]">
+                {
+                    sliderItems.map(item => (
+                        <div className={`flex items-center w-screen h-screen bg-[${item.bg}]`} key={item.id}>
+                            <div className="flex-1 h-full">
+                                <img className='h-[80%]' src={item.img} alt="" />
+                            </div>
+                            <div className="flex-1 p-12 uppercase">
+                                <h1 className="text-7xl ">{item.title}</h1>
+                                <p className="my-11 mx-0 text-xl font-semibold tracking-[3px]">{item.desc}</p>
+                                <button className="uppercase p-3 border border-black cursor-pointer">shop now</button>
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
-            {/* ====Third slide */}
-            <div className="flex items-center w-screen h-screen">
-                <div className="flex-1 h-full">
-                    <img className='h-[80%]' src="https://i.pinimg.com/originals/4a/35/63/4a3563e983a31a2001a1bfa882e2961e.png" alt="" />
-                </div>
-                <div className="flex-1 p-12 uppercase">
-                    <h1 className="text-7xl ">Popular sale</h1>
-                    <p className="my-11 mx-0 text-xl font-semibold tracking-[3px]">dont compromise on style! get flat 30% off for new arrivals</p>
-                    <button className="uppercase p-3 border border-black cursor-pointer">shop now</button>
-                </div>
+            <div
+                onClick={() => handleClick("right")}
+                className='absolute top-0 cursor-pointer opacity-70 bottom-0 m-auto right-[10px] w-[50px] h-[50px] bg-[#fff7f7] rounded-full flex items-center justify-center'>
+                <ChevronRightOutlinedIcon />
             </div>
         </div>
-        <div
-            onClick={() => handleClick("right")}
-            className='absolute top-0 cursor-pointer opacity-70 bottom-0 m-auto right-[10px] w-[50px] h-[50px] bg-[#fff7f7] rounded-full flex items-center justify-center'>
-            <ChevronRightOutlinedIcon />
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Slider
---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://i.pinimg.com/originals/4a/35/63/4a3563e983a31a2001a1bfa882e2961e.png",
+        title: "summer sale",
+        desc: "dont compromise on style! get flat 30% off for new arrivals",
+        bg: "f5fafd"
+    },
+    {
+        id: 2,
+        img: "https://i.pinimg.com/originals/4a/35/63/4a3563e983a31a2001a1bfa882e2961e.png",
+        title: "winter sale",
+        desc: "dont compromise on style! get flat 30% off for new arrivals",
+        bg: "fcf1ed"
+    },
+    {
+        id: 3,
+        img: "https://i.pinimg.com/originals/4a/35/63/4a3563e983a31a2001a1bfa882e2961e.png",
+        title: "popular sale",
+        desc: "dont compromise on style! get flat 30% off for new arrivals",
+        bg: "f5fafd"
+    },
+]
